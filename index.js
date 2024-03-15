4b8d913f9fd9a5e2bffc1a99e80ab92b96ab4ea2
MonkeyNinja2296 <111456716+MonkeyNinja2296@users.noreply.github.com>
2024-03-15 22:25:57 +0000

    Https???

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const socketio = require("socket.io");
-const http = require("http");
+const http = require("https");
 const port = 3000;
 
 const app = express();
