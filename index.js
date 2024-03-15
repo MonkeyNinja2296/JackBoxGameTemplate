a4d0c0438b702bb89764d8e7c76df04d7bd4f780
Monkeyninja2296 Cornaby <11440483-GrantCornaby@users.noreply.replit.com>
2024-03-15 22:55:05 +0000

    undo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const socketio = require("socket.io");
-const http = require("https");
+const http = require("http");
 const port = 3000;
 
 const app = express();
